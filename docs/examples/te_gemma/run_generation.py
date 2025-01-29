408eb087369725ae70ff167e8d1543a435591b56
Sudhakar Singh <sudhakars@nvidia.com>
2025-01-28 18:00:37 -0800

    remove te_gemma tutorial related files
    Signed-off-by: Sudhakar Singh <sudhakars@nvidia.com>

--- docs/examples/te_gemma/run_generation.py
+++
@@ -1,9 +0,0 @@
-from utils import *
-
-hyperparams.model_name = "/tmp/gemma-7b-hf/" # <== Add model weight location here e.g. "/path/to/downloaded/gemma/weights"
-hyperparams.qkv_format = "thd"
-
-model = init_te_gemma_model(hyperparams)
-
-print_sample_of_generated_texts(model)
-# benchmark_generation(model)
